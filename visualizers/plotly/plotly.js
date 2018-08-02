ef0a99976eb36479c62958be972af8fb3f94b653
Sylwia <s.brodacka@gmail.com>
2018-08-02 14:03:25 +0200

    fix defaults in visualizations

--- visualizers/plotly/plotly.js
+++ visualizers/plotly/plotly.js
@@ -4,19 +4,26 @@
   var defColG = 106;
   var defColB = 2;
   var defColA = 1;
+  var defSize = 10;
   var getColumn = function (array, column, defaultValue){
-    var newArr = [];
-    for (var j = 0; j<array.length; j++){
-      var x = array[j];
-      newArr.push (x[column] !== " " ? x[column] : defaultValue);
-    };
+    var newArr = Array(array.length).fill(defaultValue);
+    
+    if (column != -1) {
+      for (var row = 0; row<array.length; row++) {
+        var x = array[row][column];
+        if (x !== " ") newArr[row] = x;
+      }
+    }
     return newArr;
   };
   var getColor = function (array, r,g,b,a, defR, defG, defB, defA){
-    var newColorArr = [];
-    for (var j = 0; j<array.length; j++){
-      var x = array[j];
-      newColorArr.push (x[r] !== " " && x[g] !== " " && x[b] !== " " && x[a] !== " " ? 'rgb(' + x[r] + ',' + x[g] + ',' + x[b] + ',' + x[a] + ')'  : 'rgb(' + defR + ',' + defG + ',' + defB + ',' + defA + ')' );
+    var newColorArr = Array(array.length).fill('rgb(' + defR + ',' + defG + ',' + defB + ',' + defA + ')');
+    if ((r != -1) || (g != -1) || (b  != -1) || (a != -1)) {
+      console.log("dupa")
+      for (var j = 0; j<array.length; j++){
+        var x = array[j];
+        if (x[r] != " " && x[g] != " " && x[b] != " " && x[a] != " ") {newColorArr[j] = 'rgb(' + x[r] + ',' + x[g] + ',' + x[b] + ',' + x[a] + ')'};
+      }
     }; 
     return newColorArr;
   }
@@ -47,10 +54,11 @@
         s = header.indexOf('size')
         labels = header.indexOf('labels')
         color = getColor(dataSeries,colR, colG, colB, colA, defColR, defColG, defColB, defColA)
+
         datax = getColumn(dataSeries, posX, defPos);
         datay = getColumn(dataSeries, posY, defPos);
         dataz = getColumn(dataSeries, posZ, defPos);
-        sizes = getColumn(dataSeries, s);
+        sizes = getColumn(dataSeries, s, defSize);
         dataLabels = getColumn(dataSeries, labels);
         var trace = {
           x: datax,
@@ -65,7 +73,7 @@
           mode: "markers",
           name: "scatter1"
         };
-        // Here things to do with data
+        console.log(trace)
         Plotly.newPlot("plotly_div", [trace]);
     };
     });
